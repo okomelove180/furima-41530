3cb12cddd281f52ab3097fdd3d77c0c7c157b9b8
okomelove180 <toru.camp180@gmail.com>
2024-09-08 18:28:15 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,25 @@
 class Item < ApplicationRecord
+  # Associations
   belongs_to :user
+  has_one_attached :image
 
+  # Validations
   validates :user_id, presence: true
   validates :product_name, presence: true
   validates :product_description, presence: true
-  validates :category_id, presence: true
-  validates :product_condition_id, presence: true
-  validates :shipping_cost_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :delivery_time_id, presence: true
-  validates :price, presence: true, numericality: { only_integer: true, greater_than: 299, less_than: 10_000_000 }
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :product_condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_cost_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_time_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range (¥300~¥9,999,999)' }
 
-  has_one_attached :image
+  # ActiveHash Associations
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_genre
+  belongs_to :product_condition_genre
+  belongs_to :shipping_cost_genre
+  belongs_to :prefecture_genre
+  belongs_to :delivery_time_genre
 end
