0724c534fbff7d75794df432c542e91fe50952d6
okomelove180 <toru.camp180@gmail.com>
2024-09-10 14:35:35 +0900

    商品出品機能_02
    userに対する不要なvalidationを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,6 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   # Validations
-  validates :user_id, presence: true
   validates :product_name, presence: true
   validates :product_description, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1 }
